576f7165f8a9ceea190d1ca615192f13cbe5dc37
Raymond Jiang <hellolaojiang@gmail.com>
2019-06-20 17:17:41 +0800

    init submit

---
+++ sort/insertion_sort.go
@@ -0,0 +1,16 @@
+package main
+
+//插入排序 O(n*n)
+func InsertionSort(arrs []int) []int {
+	for i:=1; i < len(arrs); i++{
+		key := arrs[i]
+		j := i - 1
+		for ; j >=0 && arrs[j] > key ; {
+			arrs[j+1] = arrs[j]
+			j--
+		}
+		arrs[j+1] = key
+	}
+	return arrs
+}
+
---
+++ sort/insertion_sort_test.go
@@ -0,0 +1,28 @@
+package main
+
+
+import (
+	"gotest.tools/assert"
+	"fmt"
+	"math/rand"
+	"strings"
+	"testing"
+)
+
+
+func Test_InsertSort(t *testing.T) {
+	arrs := []int{1, 3, 4, 2, 8, 5, 6, 7, 9, 0}
+
+	sortedArrs := InsertionSort(arrs)
+	assert.Equal(t, strings.Trim(strings.Join(strings.Fields(fmt.Sprint(sortedArrs)),""), "[]"), "0123456789")
+}
+func Benchmark_InsertSort(b *testing.B)  {
+	b.StopTimer()
+	arrs := []int{}
+	for i:=0; i < b.N; i++ {
+		arrs = append(arrs, rand.Intn(b.N))
+	}
+
+	b.StartTimer()
+	InsertionSort(arrs)
+}
